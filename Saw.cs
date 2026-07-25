971df09c853f7f1662d025777ac253d3f2ada002
CogitoErgoSum01 <amanyadav170608@gmail.com>
2026-07-25 21:49:04 +0530

    Update Saw.cs

--- Saw.cs
+++ Saw.cs
@@ -23,7 +23,7 @@ public partial class Saw : AnimatableBody2D
     }
 }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	
 	public override void _Process(double delta)
 	{
 		
